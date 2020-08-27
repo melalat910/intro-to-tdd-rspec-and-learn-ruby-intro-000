160977f4e3c65b8220412d0ad5010db47de8fc4b
Tirzah P. Howard <tp.howard47@gmail.com>
2020-08-27 04:18:07 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,4 @@
-```ruby
-require_relative '../current_age_for_birth_year.rb'
  def current_age_for_birth_year(birth_year)
  age_of_person = current_age_for_birth_year(1984)
   2003 - birth_year
 end
-```
